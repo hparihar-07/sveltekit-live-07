0bf3b2c8dd1746879139c1168756d3f82a872ae2
hparihar-07 <hackit217@gmail.com>
2024-08-12 21:36:48 +0530

    edited server.js

--- src/routes/blog/[slug]/+page.server.js
+++ src/routes/blog/[slug]/+page.server.js
@@ -1,5 +1,7 @@
-export function load({params}){
-    return {
-        content: `Hello ${params.slug}`
-    }
+import { sql } from "@vercel/postgres";
+
+export async function load({ params, locals }) {
+  return {
+    names: await sql`SELECT * from sveltekit-live-07-postgres where user_id='${locals.user}'`
+  }
 }
